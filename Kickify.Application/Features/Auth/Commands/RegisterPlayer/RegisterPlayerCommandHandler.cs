22fcb5b1a8306f593be02525240de62ab0aacf11
Nguyen Huu Hien <hiennhse180274@fpt.edu.vn>
2026-01-07 21:20:21 +0700

    feature: login/register (player)

---
+++ Kickify.Application/Features/Auth/Commands/RegisterPlayer/RegisterPlayerCommandHandler.cs
@@ -0,0 +1,73 @@
+﻿using Kickify.Application.Abstractions.Authentication;
+using Kickify.Application.Abstractions.Messaging;
+using Kickify.Application.Abstractions.Persistence;
+using Kickify.Application.Abstractions.Repositories;
+using Kickify.Domain.Common;
+using Kickify.Domain.Entities;
+using Kickify.Domain.Enums;
+using Kickify.Domain.Errors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kickify.Application.Features.Auth.Commands.RegisterPlayer
+{
+    public class RegisterPlayerCommandHandler : ICommandHandler<RegisterPlayerCommand, RegisterPlayerCommandResponse>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IAuthenticationServices _authenticationServices;
+        private readonly IPasswordHasher _passwordHasher;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public RegisterPlayerCommandHandler(IUserRepository userRepository, IAuthenticationServices authenticationServices, IPasswordHasher passwordHasher, IUnitOfWork unitOfWork)
+        {
+            _userRepository = userRepository;
+            _authenticationServices = authenticationServices;
+            _passwordHasher = passwordHasher;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Result<RegisterPlayerCommandResponse>> Handle(RegisterPlayerCommand request, CancellationToken cancellationToken)
+        {
+            var userExist = await _userRepository.IsEmailExistsAsync(request.Email);
+            if (userExist)
+            {
+                return Result.Failure<RegisterPlayerCommandResponse>(UserErrors.EmailAlreadyExists);
+            }
+
+            var identityId = await _authenticationServices.RegisterAsync(request.Email, request.Password);
+
+            var user = new User
+            {
+                UserId = Guid.NewGuid(),
+                FullName = request.FullName,
+                Email = request.Email,
+                PasswordHash = _passwordHasher.Hash(request.Password),
+                Role = UserRole.Player,
+                IdentityId = identityId,
+                IsActive = true,
+                IsEmailVerified = true,
+                CreatedAt = DateTime.UtcNow
+            };  
+
+            await _userRepository.AddAsync(user);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            var response = new RegisterPlayerCommandResponse
+            {
+                UserId = user.UserId,
+                Email = user.Email,
+                FullName = user.FullName,
+                Role = user.Role.ToString(),
+                IdentityId = user.IdentityId,
+                IsActive = user.IsActive,
+                IsEmailVerified = user.IsEmailVerified,
+                CreatedAt = user.CreatedAt
+
+            };
+            return Result.Success(response);
+        }
+    }
+}
--- Kickify.Application/Features/Auth/Commands/RegisterPlayer/RegisterPlayerCommandResponse.cs
+++ Kickify.Application/Features/Auth/Commands/RegisterPlayer/RegisterPlayerCommandResponse.cs
@@ -4,17 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Kickify.Application.Features.Auth.Commands.Login
+namespace Kickify.Application.Features.Auth.Commands.RegisterPlayer
 {
-    public class LoginCommandResponse
+    public class RegisterPlayerCommandResponse
     {
         public Guid UserId { get; set; }
         public string Email { get; set; } = string.Empty;
         public string FullName { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
+        public string IdentityId { get; set; } = string.Empty;
         public bool IsEmailVerified { get; set; }
         public bool IsActive { get; set; }
-        public string AccessToken { get; set; } = string.Empty;
-        public string RefreshToken { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
     }
 }
