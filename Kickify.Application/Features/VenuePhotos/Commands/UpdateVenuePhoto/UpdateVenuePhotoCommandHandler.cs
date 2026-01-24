9a9ebae71c6d4ec2abed5c185075f8ad5cc19041
NhatMinh <mcpegunny@gmail.com>
2026-01-24 14:02:18 +0700

    Revert "upload and crud full for venuePhoto"
    This reverts commit d9a3a7114b9b662cb838f66e2c6f254a0b36f186.

--- Kickify.Application/Features/VenuePhotos/Commands/UpdateVenuePhoto/UpdateVenuePhotoCommandHandler.cs
+++ Kickify.Application/Features/VenuePhotos/Commands/UpdateVenuePhoto/UpdateVenuePhotoCommandHandler.cs
@@ -25,7 +25,7 @@ namespace Kickify.Application.Features.VenuePhotos.Commands.UpdateVenuePhoto
 
         public async Task<Result<UpdateVenuePhotoResponse>> Handle(UpdateVenuePhotoCommand request, CancellationToken cancellationToken)
         {
-            // Get photo with venue info for ownership check TEST
+            // Get photo with venue info for ownership check
             var photo = await _venuePhotoRepository.GetPhotoWithVenueAsync(request.PhotoId, cancellationToken);
             if (photo == null)
             {
