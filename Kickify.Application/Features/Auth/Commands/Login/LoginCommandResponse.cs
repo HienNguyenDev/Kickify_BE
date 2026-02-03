7900326c116041b461ec958cb3d146214b597e80
Nguyen Huu Hien <hiennhse180274@fpt.edu.vn>
2026-02-03 23:49:14 +0700

    feature: login with firebase (for venue owner)

--- Kickify.Application/Features/Auth/Commands/Login/LoginCommandResponse.cs
+++ Kickify.Application/Features/Auth/Commands/Login/LoginCommandResponse.cs
@@ -12,6 +12,7 @@ namespace Kickify.Application.Features.Auth.Commands.Login
         public string Email { get; set; } = string.Empty;
         public string FullName { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
+        public string AvatarUrl { get; set; } = string.Empty;
         public bool IsEmailVerified { get; set; }
         public bool IsActive { get; set; }
         public string AccessToken { get; set; } = string.Empty;
