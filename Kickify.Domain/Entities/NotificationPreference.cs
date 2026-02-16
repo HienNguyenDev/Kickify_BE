fad8ca428af37ec262b276d5ad51f6fcc4a723da
Nguyen Huu Hien <hiennhse180274@fpt.edu.vn>
2026-02-16 19:21:41 +0700

    update: notification preferences

--- Kickify.Domain/Entities/NotificationPreference.cs
+++ Kickify.Domain/Entities/NotificationPreference.cs
@@ -6,11 +6,9 @@ public class NotificationPreference : BaseEntity
 {
     public Guid PreferenceId { get; set; }
     public Guid UserId { get; set; }
-    public bool MatchInvites { get; set; } = true;
-    public bool MatchResults { get; set; } = true;
-    public bool ChatMessages { get; set; } = true;
-    public bool RoomUpdates { get; set; } = true;
-    public bool SystemAnnouncements { get; set; } = true;
+    public bool MatchRoom { get; set; } = true;
+    public bool Friendship { get; set; } = true;
+    public bool Post { get; set; } = true;
 
     // Navigation properties
     public User User { get; set; } = null!;
